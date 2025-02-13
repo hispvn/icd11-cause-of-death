c15ee9d038d6c97d7771fd39251c3da434e8dd9b
Thai Nguyen <duythai275@gmail.com>
2025-02-13 10:33:07 -0700

    SECURITY CHECK: Remove the clientId and clientSecret for Access Token to ICD11 Api

--- src/utils/icd11.js
+++ src/utils/icd11.js
@@ -1,31 +1,5 @@
 let apiUrl = process.env.REACT_APP_ICD11_API_URL;
 
-const getToken = () => {
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-  var urlencoded = new URLSearchParams();
-  urlencoded.append(
-    "client_id",
-    "2d53eefc-1747-46db-a8ec-8836fdd6fea6_ba036e00-644d-4737-9379-276a7a3d8fd1"
-  );
-  urlencoded.append(
-    "client_secret",
-    "ZDCXrnifxIUV132pfjKFncHFYsbqZF4vmOH/rU9xpwc="
-  );
-  urlencoded.append("scope", "icdapi_access");
-  urlencoded.append("grant_type", "client_credentials");
-  var requestOptions = {
-    method: "POST",
-    headers: myHeaders,
-    body: urlencoded,
-    redirect: "follow"
-  };
-  fetch("https://icdaccessmanagement.who.int/connect/token", requestOptions)
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.log("error", error));
-};
-
 const searchByCode = (keyword) => {
   let url = apiUrl + "/icd/release/11/2020-09/mms/codeinfo/";
   url += keyword;
